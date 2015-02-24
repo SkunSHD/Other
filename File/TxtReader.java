ea135b4cd05f59ef57c3fe936a60795d4f451885
SKUNS <skunshd@gmail.com>
2015-02-24 17:57:48 +0200

    TxtReader changes1

--- File/TxtReader.java
+++ File/TxtReader.java
@@ -1,5 +1,7 @@
-import java.swing.*;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 /**
 * Sources:
 * http://math.sgu.ru/sites/chairs/prinf/materials/java/lesson8.htm
@@ -26,7 +28,7 @@ public class TxtReader extends JFrame {
 		Container c = getContentPane();
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 5));
 
-		JButton pbn1 = new JButton("Open");
+		JButton btn1 = new JButton("Open");
 		panel.add(btn1);
 		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -34,7 +36,67 @@ public class TxtReader extends JFrame {
 			}
 		});
 
+		JButton btn2 = new JButton("Save");
+		panel.add(btn2);
+		btn2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				save();
+			}
+		});
+
+		jta = new JTextArea();
+		JScrollPane scrollArea = new JScrollPane(jta);
+		c.add(scrollArea, BorderLayout.CENTER);
+
+		c.add(panel, BorderLayout.NORTH);
+
+		WindowListener wndCloser = new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		};
+		addWindowListener(wndCloser);
+
+		setVisible(true);
 	}
 
+	public void open() {
+		JFileChooser fc = new JFileChooser();
+		ExtFileFilter ff1 = new ExtFileFilter("txt", "*.txt - text files");
+		fc.addChoosableFileFilter(ff1);
+
+		ExrFileFilter eff2 = new ExtFileFilter("java", "*.java - java programms");
+		fc.addChoosableFileFilter(ff2);
+
+		if(fc.showOpenDialog(this)) == JFileChooser.APPROVE_OPTION) {
+			File f = fc.getSelectdFile();
+		}
+	}
+
+	public void save() {
+
+	}
+
+	public class ExtFileFilter extends javax.swing.filechooser.FileFilter {
+		String ext;
+		String description;
+
+		ExtFileFilter(String ext, String description) {
+			this.ext = ext;
+			this.description = description;
+		}
+
+		public boolean accept(File f) {
+			String ext = getExtention(f);
+		}
+
+		public String getExtention(File f) {
+			String s = f.getName();
+		}
+	}
+
+	public static void main(String[] args) {
+		new TxtReader();
+	}
 
 }
